eae2ad9e0035387cd6a234165681e2eb5c994d9f
Nitish Kumar <erkumarnitish@gmail.com>
2016-04-19 16:41:40 +0530

    V3.0 Production

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,8 +14,6 @@ public class App {
         ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/wordcount/*");
         jerseyServlet.setInitOrder(1);
         jerseyServlet.setInitParameter("jersey.config.server.provider.packages","com.nitish.freecharge.resources");
-        /*jerseyServlet.setInitParameter("jersey.config.server.provider.classnames",
-                "org.glassfish.jersey.moxy.json.MoxyJsonFeature");*/
         try {
 			System.out.println("Starting the server..");
 			server.start();
